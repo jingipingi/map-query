f7b9c8e2866e3c816e4fb11cc0416c553102f539
jewleeahnaw <juwnoronha@gmail.com>
2023-11-12 22:11:58 -0800

    Neighborhoods are now colored by rent index from Sept, 2023. The rent data is a static CSV, however, and would be better pulled from an API.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import time
 import folium
 import googlemaps
 import pandas as pd
+import geopandas as gpd
+import branca
+import matplotlib.pyplot as plt  # Add this line
+import seaborn as sns
+import numpy as np
+# from matplotlib.colors import rgb2hex
 
 
 def get_city_info(city_name, api_key):
@@ -26,7 +32,7 @@ def gen_params(city_info, keyword_search):
     }
 
 
-def get_all_places(params, api_key):
+def get_all_places(params, city_name, api_key):
     gmaps = googlemaps.Client(key=api_key)
     all_places = pd.DataFrame(columns=['name', 'lat', 'lon', 'category'])
     next_page_token = None
@@ -37,12 +43,11 @@ def get_all_places(params, api_key):
             params['page_token'] = next_page_token
 
         try:
-            print("params: ", params)
             places_result = gmaps.places_nearby(**params)
         except Exception as e:
-            print("error: ", e)
             break
         places = places_result.get('results', [])
+        places = [place for place in places_result["results"] if city_name in place["vicinity"]]
 
         current_places = pd.DataFrame({
             'name': [place['name'] for place in places],
@@ -63,15 +68,15 @@ def get_all_places(params, api_key):
 # Example usage:
 
 api_key = 'AIzaSyCAsZO05dTX_K5GA_JS_WvsJdgh-3qyDAA'
-city_info = get_city_info('San Francisco', api_key)
-categories = ['Pharmacy', 'Gym','Trader Joes','Safeway','Park']
+city_name = 'San Francisco'
+city_info = get_city_info(city_name, api_key)
+categories = ['Pharmacy', 'Gym','Trader Joes','Safeway','Park', 'Coffee shop']
 result_df = pd.DataFrame()
 
 for category in categories:
     initial_params = gen_params(city_info, category)
-    cat_df = get_all_places(initial_params, api_key)
+    cat_df = get_all_places(initial_params, city_name, api_key)
     result_df = pd.concat([result_df, cat_df], ignore_index=True, sort=False)
-    print(result_df)
 
 data = result_df
 
@@ -106,6 +111,44 @@ for index, row in result_df.iterrows():
     ).add_to(m)
 
 #TODO: color neighborhood regions by median rental price for a 2b1b
+'https://api.bridgedataoutput.com/api/v2/zgecon/{endpoint}?access_token={access_token}&metadataType={metadata}'
+
+# Load neighborhood boundary GeoJSON data from the JSON file
+neighborhoods_json = "SanFrancisco.Neighborhoods.json"
+neighborhoods_gdf = gpd.read_file(neighborhoods_json)
+
+# Read rent values from a CSV file
+rent_data = pd.read_csv("neighborhood_rent.csv")  # Replace with the actual file path
+
+# Merge rent data with neighborhood geometries
+merged_data = neighborhoods_gdf.merge(rent_data, on="neighborhood", how="left")
+print(merged_data)
+
+cp = folium.Choropleth(
+    geo_data = neighborhoods_json,
+    name ='choropleth',
+    data = merged_data,
+    columns = ['neighborhood','rent'],
+    key_on='feature.properties.neighborhood',
+    fill_color = 'YlGnBu',     #I passed colors Yellow,Green,Blue
+    fill_opacity = 0.7,
+    line_opacity = 0.2,
+   legend_name = "Rent Index"
+).add_to(m)
+
+# Iterate through each neighborhood feature and add it to the map with a Popup label and color based on rent
+for idx, row in merged_data.iterrows():
+    neighborhood_name = row["neighborhood"]
+    neighborhood_geometry = row["geometry"]
+    rent_value = row["rent"]
+    print(rent_value)
+
+    # Create a GeoJson object with a Popup label and custom style
+    folium.GeoJson(
+        neighborhood_geometry,
+        name=neighborhood_name
+    ).add_child(folium.Popup('name: ' + neighborhood_name + ', rent: ' + str(rent_value))).add_to(m)
+
 #TODO: filter the results to be confined to the city that was searched
 
 # Display the map
